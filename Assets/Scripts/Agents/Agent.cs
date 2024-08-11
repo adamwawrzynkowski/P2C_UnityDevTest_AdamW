af69d2d1e9bda8350258f12beafd2ea75fca8bd2
Adam Wawrzynkowski <adamwawrzynkowski@icloud.com>
2024-08-11 15:30:38 +0200

    Cleanup and comments

--- Assets/Scripts/Agents/Agent.cs
+++ Assets/Scripts/Agents/Agent.cs
@@ -24,14 +24,18 @@ namespace Agents {
         private int currentPathIndex = 0;
 
         private void Awake() {
+            // Assign the components
             lr = GetComponent<LineRenderer>();
             animator = GetComponentInChildren<Animator>();
             seeker = GetComponent<Seeker>();
         }
 
         private void Update() {
+            // Always look at the target point
             transform.DOLookAt(destinationPosition, 0.1f, AxisConstraint.Y);
             
+            // Check if the path exists and update its visuals
+            // Will update only when TAB is pressed (Line Renderer is enabled)
             if (lr == null || path == null) return;
             if (UIManager.Instance.GetTabPressed()) {
                 if (!lr.enabled) lr.enabled = true;
@@ -42,16 +46,19 @@ namespace Agents {
             if (lr.enabled) lr.enabled = false;
         }
 
+        // Find a random point inside a box bounds
         public Vector3 FindPoint(BoxCollider area) {
             var randomBoundsX = Random.Range(area.bounds.min.x, area.bounds.max.x);
             var randomBoundsZ = Random.Range(area.bounds.min.z, area.bounds.max.z);
             return new Vector3(randomBoundsX, 0.0f, randomBoundsZ);
         }
 
+        // Find a path using A* Algorithm
         private void FindPath(BoxCollider area) {
             seeker.StartPath(transform.position, FindPoint(area), PathFound);
         }
 
+        // Confirm path
         private void PathFound(Path _path) {
             if (_path.error) {
                 Debug.LogError("Path failed: " + _path.errorLog);
@@ -64,12 +71,15 @@ namespace Agents {
             GoToDestination();
         }
 
+        // Tell the agent to move towards path points
         public void GoToDestination() {
+            // If the path is null - find it
             if (path == null) {
                 FindPath(AgentsManager.Instance.GetArea());
                 return;
             }
             
+            // If the path is finished - find a new one
             if (currentPathIndex >= path.Length || currentPathIndex == -1) {
                 path = null;
                 
@@ -77,11 +87,13 @@ namespace Agents {
                 return;
             }
             
+            // Set the destination points
             destinationPosition = path[currentPathIndex];
             currentPathIndex++;
             
             lr.positionCount = path.Length + 1 - currentPathIndex;
             
+            // Set a tweener to move towards point
             tweener = transform.DOMove(
                 destinationPosition, 
                 GetDistance() / IAgentService.DefaultAgentMoveDuration
@@ -90,6 +102,7 @@ namespace Agents {
             UpdateTweener();
         }
 
+        // Mark point as reached
         private void GoalCompleted() {
             GoToDestination();
             
@@ -97,16 +110,19 @@ namespace Agents {
             UIManager.Instance.AddToConsole("Agent " + GUID + " arrived.");
         }
 
+        // Unregister and kill agent
         public void Unregister() {
             tweener.Kill();
             Destroy(gameObject);
         }
 
+        // Update the tweener timeScale and animation speed based on the given simulation speed
         public void UpdateTweener() {
             tweener.timeScale = TickManager.Instance.GetCurrentTickSpeed();
             animator.speed = IAgentService.DefaultAnimationSpeed * TickManager.Instance.GetCurrentTickSpeed();
         }
 
+        // Check distance to target
         private float GetDistance() {
             return Vector3.Distance(transform.position, destinationPosition);
         }
--- Assets/Scripts/Agents/AgentsManager.cs
+++ Assets/Scripts/Agents/AgentsManager.cs
@@ -29,6 +29,7 @@ namespace Agents {
         private List<Agent> registeredAgents = new List<Agent>();
         public int GetAgentsCount() => registeredAgents.Count;
 
+        // Spawn new agent and register him
         public void RequestAgentSpawn() {
             var iterations = Input.GetKey(KeyCode.LeftShift) ? 10 : 1;
             for (var i = 0; i < iterations; i++) {
@@ -46,6 +47,7 @@ namespace Agents {
             }
         }
 
+        // Remove agent based on the given settings
         public void RequestAgentRemove(AgentRemoveType type) {
             if (registeredAgents == null || registeredAgents.Count <= 0) return;
             switch (type) {
@@ -62,6 +64,7 @@ namespace Agents {
             }
         }
         
+        // Register new agent and assign an GUID to him
         private void RegisterAgent(Agent agent) {
             var newGUID = Guid.NewGuid();
             while (CheckGUIDExists(newGUID)) {
@@ -74,6 +77,7 @@ namespace Agents {
             UIManager.Instance.RefreshAgentsUI();
         }
 
+        // Unregister agent and remove him from the list
         private void UnregisterAgent(Agent agent, bool modifyCollection = true) {
             if (modifyCollection) registeredAgents.RemoveAt(registeredAgents.FindIndex(a => agent == a));
             agent.Unregister();
@@ -81,6 +85,7 @@ namespace Agents {
             UIManager.Instance.RefreshAgentsUI();
         }
 
+        // Unregister all agents on the scene
         private void RemoveAllAgents() {
             foreach (var agent in registeredAgents.Where(agent => agent != null)) {
                 UnregisterAgent(agent, false);
@@ -90,6 +95,7 @@ namespace Agents {
             UIManager.Instance.RefreshAgentsUI();
         }
 
+        // Check if this GUID already exists on the list
         private bool CheckGUIDExists(Guid guid) {
             foreach (var _ in registeredAgents.Where(agent => agent.GetGUID() == guid)) {
                 return true;
